48ff4c2df666c01686c0b6e727ef1b81be51e043
David Vaz <david.vaz@cuelogic.co.in>
2014-10-08 14:49:58 +0530

    Commented out authenticator middleware

--- app/middleware/middleware.js
+++ app/middleware/middleware.js
@@ -1,14 +1,13 @@
 var express = require('express');
 
 exports.authenticator= function(req,res,next){  
-  var path=req.path;
-  if (path.substring(path.lastIndexOf("/"))==='/sign_in' || req.session.email){
-  	console.log('valid');
-  	console.log(req.session.email);
+  // var path=req.path;
+  // if (path.substring(path.lastIndexOf("/"))==='/sign_in' || req.session.email){
+  // 	console.log('valid');
+  // 	console.log(req.session.email);
   	next();	
-  }else{
-  	console.log('in valid');
-  	next('Not Authorised');	
-  }
-  
+  // }else{
+  // 	console.log('in valid');
+  // 	next('Not Authorised');	
+  // }
 }
